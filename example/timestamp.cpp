3a5943687ffb7a0e2d8fc082cf7cc02d34aa1d7a
klemens-morgenstern <klemens.morgenstern@gmx.net>
2017-05-30 23:54:41 +0200

    updated doc

--- example/timestamp.cpp
+++ example/timestamp.cpp
@@ -2,9 +2,17 @@
 #include <mw/test/calltrace.hpp>
 #include <chrono>
 
+extern "C" { extern int __mw_calltrace_size; }
+
 mw::test::timestamp_t mw::test::timestamp()
 {
-    return std::chrono::system_clock::now()
+    int sz = __mw_calltrace_size;
+    __mw_calltrace_size = 0;
+
+    auto ts = std::chrono::system_clock::now()
                 .time_since_epoch().count();
+
+    __mw_calltrace_size = sz;
+    return ts;
 }
 //]
